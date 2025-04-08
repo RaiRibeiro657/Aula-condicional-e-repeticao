a9af01a66191ca74491653ffea05a0fc3b223139
Gabriel <gabrielgibin0504@gmail.com>
2025-04-08 14:26:03 -0300

    Programa do login simples feito

---
+++ loginsimples.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int senha_certa = 4321;
+    int senha_digitada;
+
+    printf("Sistema de Login\n");
+
+    do {
+        printf("Digite a senha de 4 digitos: ");  
+
+        if (senha_digitada != senha_certa) {
+            printf("Senha incorreta! Tente denovo.\n"); 
+        }
+
+    } while (senha_digitada != senha_certa);  
+    
+    printf("Login realizado com sucesso!\n");
+    
+    return 0;
+}
