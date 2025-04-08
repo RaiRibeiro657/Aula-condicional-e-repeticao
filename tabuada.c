408cee52ca893c47a16b00ba984690ddbe98688c
Igor <RM564130@fiap.com.br>
2025-04-08 10:30:36 -0300

    Tabuada 1.0

--- tabuada.c
+++ tabuada.c
@@ -6,21 +6,27 @@ int tabuada()
 
     while (1)
     {
-        printf("Digite um numero de 1 a 10:");
-        scanf("%d, &numero");
+        printf("Digite um numero de 1 a 10: ");
+        scanf("%d", &numero); // Corrigido: Removido a vírgula e adicionado o & corretamente
 
         if (numero >= 1 && numero <= 10)
         {
             break;
         }
-        printf("Número inválido! Tente novamente. /n");
+        printf("Número inválido! Tente novamente.\n"); // Corrigido: Alterado /n para \n
     }
 
-    // imprime a tabuadado número válido
-    printf("Tabuada de %d/n, numero");
+    // Imprime a tabuada do número válido
+    printf("Tabuada de %d:\n", numero); // Corrigido: Alterado /n para \n e corrigido o formato
     for (int i = 1; i <= 10; i++)
     {
-        printf("%d x %d = %d", numero, i, numero * i);
+        printf("%d x %d = %d\n", numero, i, numero * i); // Corrigido: Adicionado \n para nova linha
     }
+    return 0;
+}
+
+int main()
+{
+    tabuada(); // Chama a função tabuada
     return 0;
 }
